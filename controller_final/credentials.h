deddaa42c2e208377e52b3d16c6c64cc8f492114
IvFrozen <ivan.khrulenko@gmail.com>
2020-08-22 18:55:41 -0700

    Add files via upload
    added credentials.h

---
+++ controller_final/credentials.h
@@ -0,0 +1,8 @@
+#define mqtt_server "xxxx" // the MQTT server address
+#define sensor_mqtt_user "xxxx"           // the user login
+#define controller_mqtt_user "xxxx"           // the user login
+#define mqtt_password "xxxx"           // the password
+#define weather_key "xxxx" //API key for weather conditions
+#define wifi_ssid "xxxx" // YOUR network name
+#define wifi_password "xxxx" // YOUR network password
+#define irr_controller_url "xxxx" //Irrigation controller URL
